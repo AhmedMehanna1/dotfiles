e2079742bd151417ef15d7f497b8cf6d49093d1d
Ahmed Mehanna <ahmed.mehanna@hypercell.net>
2025-06-18 10:04:38 +0300

    update tmux installation script

--- tmux/install.sh
+++ tmux/install.sh
@@ -2,8 +2,10 @@
 
 
 mkdir -p ~/.tmux
-ln -s ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
-ln -s ~/.dotfiles/tmux/tmux-status.conf ~/.tmux/tmux-status.conf
+ln -fvs ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
+ln -fvs ~/.dotfiles/tmux/tmux-status.conf ~/.tmux/tmux-status.conf
+
+rm -rf ~/.tmux/plugins/tpm ~/.config/tinted-theming/base16-fzf
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 git clone https://github.com/tinted-theming/base16-fzf.git ~/.config/tinted-theming/base16-fzf
