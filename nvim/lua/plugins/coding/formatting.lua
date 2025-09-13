6b6f247cdbcb859f46ede3d8adb9d92094642ab5
Ahmed Mehanna <ahmadabobakr1@gmail.com>
2025-09-13 03:00:53 +0300

    update lsp configurations to solve Java LSP issues

--- nvim/lua/plugins/coding/formatting.lua
+++ nvim/lua/plugins/coding/formatting.lua
@@ -1,52 +1,37 @@
 return {
-  "stevearc/conform.nvim",
-  dependencies = { "mason.nvim" },
-  lazy = true,
-  cmd = "ConformInfo",
-  keys = {
-    {
-      "<leader>cf",
-      function()
-        require("conform").format({ timeout_ms = 3000, lsp_fallback = true })
-      end,
-      mode = { "n", "v" },
-      desc = "Format Document",
+    "stevearc/conform.nvim",
+    dependencies = { "mason.nvim" },
+    lazy = true,
+    cmd = "ConformInfo",
+    keys = {
+        {
+            "<leader>cf",
+            function()
+                require("conform").format({ timeout_ms = 3000, lsp_fallback = true })
+            end,
+            mode = { "n", "v" },
+            desc = "Format Document",
+        },
     },
-  },
-  init = function()
-    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-  end,
-  opts = {
-    formatters_by_ft = {
-      lua = { "stylua" },
-      python = { "isort", "black" },
-      rust = { "rustfmt" },
-      java = { "google-java-format" },
-      javascript = { { "prettierd", "prettier" } },
-      typescript = { { "prettierd", "prettier" } },
-      json = { { "prettierd", "prettier" } },
-      go = { "goimports", "gofmt" },
+    init = function()
+        vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+    end,
+    opts = {
+        formatters_by_ft = {
+            lua = { "stylua" },
+            python = { "isort", "black" },
+            rust = { "rustfmt" },
+            java = { "clang-format" }, -- ✅ use clang-format
+            javascript = { { "prettierd", "prettier" } },
+            typescript = { { "prettierd", "prettier" } },
+            json = { { "prettierd", "prettier" } },
+            go = { "goimports", "gofmt" },
+        },
+        formatters = {
+            injected = { options = { ignore_errors = true } },
+            ["clang-format"] = {
+                prepend_args = { "-style=file" }, -- look for .clang-format
+            },
+        },
     },
-    formatters = {
-      injected = { options = { ignore_errors = true } },
-
-      -- Java formatter
-      ["google-java-format"] = {
-        prepend_args = { "--aosp" }, -- optional: 4 spaces, 100 line length
-      },
-
-      -- JS/TS/JSON formatter overrides
-      prettierd = {
-        prepend_args = { "--tab-width", "4" },
-      },
-      prettier = {
-        prepend_args = { "--tab-width", "4" },
-      },
-
-      -- Rustfmt (respects rustfmt.toml if present)
-      rustfmt = {
-        prepend_args = { "--edition", "2021" },
-      },
-    },
-  },
 }
--- nvim/lua/plugins/editor/neo-tree.lua
+++ nvim/lua/plugins/editor/neo-tree.lua
@@ -8,12 +8,56 @@ return {
         "MunifTanjim/nui.nvim",
     },
     keys = {
-        { "<leader>fe", function() require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() }) end,          desc = "Explorer NeoTree (cwd)" },
-        { "<leader>fE", function() require("neo-tree.command").execute({ toggle = true, dir = require("utils").root() }) end, desc = "Explorer NeoTree (root dir)" },
-        { "<leader>e",  "<leader>fe",                                                                                         desc = "Explorer NeoTree (cwd)",      remap = true },
-        { "<leader>E",  "<leader>fE",                                                                                         desc = "Explorer NeoTree (root dir)", remap = true },
-        { "<leader>ge", function() require("neo-tree.command").execute({ source = "git_status", toggle = true }) end,         desc = "Git explorer" },
-        { "<leader>be", function() require("neo-tree.command").execute({ source = "buffers", toggle = true }) end,            desc = "Buffer explorer" },
+        {
+            "<leader>fe",
+            function()
+                local ok, nt = pcall(require, "neo-tree.command")
+                if ok then
+                    nt.execute({ toggle = true, dir = vim.loop.cwd() })
+                else
+                    vim.notify("Neo-tree not loaded", vim.log.levels.WARN)
+                end
+            end,
+            desc = "Explorer NeoTree (cwd)",
+        },
+        {
+            "<leader>fE",
+            function()
+                local ok, nt = pcall(require, "neo-tree.command")
+                if ok then
+                    nt.execute({ toggle = true, dir = require("utils").root() })
+                else
+                    vim.notify("Neo-tree not loaded", vim.log.levels.WARN)
+                end
+            end,
+            desc = "Explorer NeoTree (root dir)",
+        },
+        { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (cwd)", remap = true },
+        { "<leader>E", "<leader>fE", desc = "Explorer NeoTree (root dir)", remap = true },
+        {
+            "<leader>ge",
+            function()
+                local ok, nt = pcall(require, "neo-tree.command")
+                if ok then
+                    nt.execute({ source = "git_status", toggle = true })
+                else
+                    vim.notify("Neo-tree not loaded", vim.log.levels.WARN)
+                end
+            end,
+            desc = "Git explorer",
+        },
+        {
+            "<leader>be",
+            function()
+                local ok, nt = pcall(require, "neo-tree.command")
+                if ok then
+                    nt.execute({ source = "buffers", toggle = true })
+                else
+                    vim.notify("Neo-tree not loaded", vim.log.levels.WARN)
+                end
+            end,
+            desc = "Buffer explorer",
+        },
     },
     deactivate = function()
         vim.cmd([[Neotree close]])
@@ -22,7 +66,10 @@ return {
         if vim.fn.argc(-1) == 1 then
             local stat = vim.loop.fs_stat(vim.fn.argv(0))
             if stat and stat.type == "directory" then
-                require("neo-tree")
+                -- delay loading so lazy.nvim finishes first
+                vim.schedule(function()
+                    require("neo-tree")
+                end)
             end
         end
     end,
@@ -41,6 +88,7 @@ return {
                     local node = state.tree:get_node()
                     local path = node:get_id()
                     vim.fn.setreg("+", path, "c")
+                    vim.notify("Copied path: " .. path, vim.log.levels.INFO)
                 end,
             },
         },
@@ -55,6 +103,8 @@ return {
     },
     config = function(_, opts)
         require("neo-tree").setup(opts)
+
+        -- Refresh git status after closing lazygit terminal
         vim.api.nvim_create_autocmd("TermClose", {
             pattern = "*lazygit",
             callback = function()
