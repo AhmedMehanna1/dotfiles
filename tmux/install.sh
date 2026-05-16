ebabf9c1dca1443caafe2e9b854c81c596c6d163
Ahmed Mehanna <ahmadabobakr1@gmail.com>
2026-05-16 05:13:58 +0300

    update tmux configurations, add git configurations

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+rm -rf ~/.config/tmux
 
-mkdir -p ~/.tmux
-ln -fvs ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
-ln -fvs ~/.dotfiles/tmux/tmux-status.conf ~/.tmux/tmux-status.conf
+ln -fvs ~/.dotfiles/tmux ~/.config/tmux
 
-rm -rf ~/.tmux/plugins/tpm ~/.config/tinted-theming/base16-fzf
-
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-git clone https://github.com/tinted-theming/base16-fzf.git ~/.config/tinted-theming/base16-fzf
+#mkdir -p ~/.tmux
+#ln -fvs ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
+#ln -fvs ~/.dotfiles/tmux/tmux-status.conf ~/.tmux/tmux-status.conf
+#
+#rm -rf ~/.tmux/plugins/tpm ~/.config/tinted-theming/base16-fzf
+#
+#git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+#git clone https://github.com/tinted-theming/base16-fzf.git ~/.config/tinted-theming/base16-fzf
